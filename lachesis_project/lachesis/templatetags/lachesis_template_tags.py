7885208caaa0ec21106f412d9eb6782c627be0cc
Jordan Kalfas <jordan.kalfas97@gmail.com>
2017-03-21 17:22:17 +0000

    removed secondary project folder

--- lachesis_project/lachesis/templatetags/lachesis_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from lachesis.models import Genre
-
-register = template.Library()
-
-@register.inclusion_tag('lachesis/gens.html')
-def get_genre_list(gen=None):
-    return {'gens': Genre.objects.all(),
-        'act_gen': gen}
