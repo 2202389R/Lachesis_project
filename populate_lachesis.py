9af8c093a1d46a90b76275ff7174756c81601aa6
2176708c <2176708c@student.gla.ac.uk>
2017-03-22 20:51:31 +0000

    Edited the population script

--- populate_lachesis.py
+++ populate_lachesis.py
@@ -3,71 +3,75 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'Lachesis_group_project.settings')
 import django
 django.setup()
-from lachesis.models import Category, Page
+from lachesis.models import Genre, Story, Segment
 
 def populate():
      # First, we will create lists of dictionaries containing the pages
-     # we want to add into each category.
-     # Then we will create a dictionary of dictionaries for our categories.
+     # we want to add into each genre.
+     # Then we will create a dictionary of dictionaries for our genres.
      # This might seem a little bit confusing, but it allows us to iterate
      # through each data structure, and add the data to our models.
 
-     python_pages = [
-         {"title": "Official Python Tutorial",
-          "url":"http://docs.python.org/2/tutorial/"},
-         {"title": "How to Think like a Computer Scientist",
-          "url":"http://www.greenteapress.com/thinkpython/"},
-         {"title":"Learn Python in 10 Minutes",
-          "url":"http://www.korokithakis.net/tutorials/python/"} ]
+     fairytale_stories = [
+          {"title": "Cinderella", "author": "Charles Perrault",
+           "completed": True, "votes": 54}]
 
-     django_pages = [
-         {"title": "Official Django Tutorial",
-          "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-         {"title":"Django Rocks",
-          "url":"http://www.djangorocks.com/"},
-         {"title":"How to Tango with Django",
-          "url":"http://www.tangowithdjango.com/"} ]
+     gens = {"Fairy Tales":{"stories":fairytale_stories, "votes":54}}
 
-     other_pages = [
-         {"title":"Bottle",
-          "url":"http://bottlepy.org/docs/dev/"},
-         {"title":"Flask",
-          "url":"http://flask.pocoo.org"} ]
+     segments = {"Cinderella":{{"segment":1, "text": "hi", "Published": "2015-03-21"
+          "closed": True, "option1": "yes", "option2": "no", "option1votes": 34
+                    "option2votes":65}, {"segment":2, "text": "bye", "Published": "2015-04-21"
+          "closed": True, "option1": "haha", "option2": "oh", "option1votes": 34
+                    "option2votes":65}}}
 
-     cats = {"Python": {"pages":python_pages,"views":128,"likes":64},
-             "Django": {"pages":django_pages,"views":64,"likes":32},
-             "Other Frameworks": {"pages":other_pages,"views":32,"likes":16}}
 
-      # If you want to add more catergories or pages,
-      # add them to the dictionaries above.
-
-      # The code below goes through the cats dictionary, then adds each category,
-      # and then adds all the associated pages for that category.
-      # if you are using Python 2.x then use cats.iteritems() see
+      # The code below goes through the gens dictionary, then adds each genre,
+      # and then adds all the associated stories for that genre.
+      # if you are using Python 2.x then use gens.iteritems() see
       # http://docs.quantifiedcode.com/python-anti-patterns/readability/
       # for more information about how to iterate over a dictionary properly.
 
 
-     for cat, cat_data in cats.items():
-         c = add_cat(cat,cat_data["views"],cat_data["likes"])
-         for p in cat_data["pages"]:
-             add_page(c, p["title"],p["url"])
+     for gen, gen_data in gens.items():
+         g = add_gen(gen,gen_data["votes"])
+         for s in gen_data["stories"]:
+             add_story(g, s["title"],s["author"], s["completed"])
+
+     for se in segments.items():
+          if segments[se] = s["title"]:
+               add_segment(segments[se],segments[se]["segment"],
+                    segments[se]["text"], segment[se]["pub_date"],
+                    segment[se]["option1"],segment[se]["option2"],
+                    segment[se]["option1votes"],segment[se]["option2votes"])
+     
+     for s in Story.objects.all():
+         for se in segments.items():
+              
+             add_segment(s, se["text"], se["pub_date"], se["option1"], se["option2"],
+                         se["option1votes"], se["option2votes"])
 
      #print out the categories we have added
-     for c in Category.objects.all():
-        for p in Page.objects.filter(category=c):
-            print ("- {0} - {1}".format(str(c),str(p)))
+     for g in Genre.objects.all():
+        for s in Story.objects.filter(genre=g):
+            print ("- {0} - {1}".format(str(g),str(s)))
 
+def add_segment(story, segment, text, pub_date=datetime.date.today, option1,
+                option2, option1votes= 0, option2votes=0):
+    s = Segment.objects.get_or_create(story=story, segment=segment, segment_text=text, pub_date=pub_date,
+                                      option1=option1, option2=option2,
+                                      option1votes=option1votes, option2votes=option2votes)[0]
+    s.save()
+    return s
 
-def add_page(cat, title, url, views = 0):
-    p = Page.objects.get_or_create(category=cat, title = title, url=url, views=views)[0]
-    p.save()
-    return p
+def add_story(gen, title, author, completed = False, votes = 0):
+    s = Story.objects.get_or_create(genre=gen, title = title, author=author, completed=completed, votes=votes)[0]
+    s.save()
+    return s
 
-def add_cat(name,views=0,likes=0):
-    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
-    c.save()
-    return c
+def add_gen(name,votes=0):
+    g = Genre.objects.get_or_create(name=name,votes=votes)[0]
+    g.save()
+    return g
 
 #Start execution here!
 if __name__ == '__main__':
