26951ea24d9c798f1016609838a9a7dcb62ab726
Georgia Constantinou <Georgia Constantinou>
2017-03-22 12:52:44 +0000

    Edited the admin file

--- lachesis/admin.py
+++ lachesis/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from lachesis.models import Category, Page
+from lachesis.models import Genre, Story, Segment
 from lachesis.models import UserProfile
 
-class CategoryAdmin(admin.ModelAdmin):
+class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name', )}
 
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'author')
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ('segment', 'story', 'closed')
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Story, StoryAdmin)
+admin.site.register(Segment, SegmentAdmin)
 admin.site.register(UserProfile)
